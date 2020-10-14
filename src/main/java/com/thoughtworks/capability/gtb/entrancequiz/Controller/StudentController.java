321dad90071c1391c12344910649c94413dc6732
zhuyuqian <yuqian.zhu@thoughtworks.com>
2020-10-14 11:46:00 +0800

    [zhuyuqian]GTB comments

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/Controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/Controller/StudentController.java
@@ -1,3 +1,4 @@
+//TODO GTB: 包名应小写
 package com.thoughtworks.capability.gtb.entrancequiz.Controller;
 
 import com.thoughtworks.capability.gtb.entrancequiz.Model.Student;
@@ -19,6 +20,7 @@ public class StudentController {
 
     @GetMapping("/students")
     @CrossOrigin
+    //TODO GTB: 跨域配置应尽量避免使用通配符，应指定特定的域名
     public List<Student> findAllStudents() {
         return studentService.findAll();
     }
